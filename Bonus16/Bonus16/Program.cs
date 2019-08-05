8f41e1ed7af0f6a8058d50fa8dc3fb7d42888f30
BluEyesWhiteJason <jason.speet@gmail.com>
2019-08-04 22:09:26 -0400

    did most of bonus17 stuff

--- Bonus16/Bonus16/Program.cs
+++ Bonus16/Bonus16/Program.cs
@@ -29,7 +29,7 @@ namespace Bonus16
             for (int i = 0; i < num; i++)
             {
                 Console.WriteLine($"Car {i + 1}");
-               
+
 
                 Console.Write($"Enter Car #{i + 1} Make: ");
                 string make = myTI.ToTitleCase(Console.ReadLine());
@@ -37,29 +37,103 @@ namespace Bonus16
                 Console.Write($"Enter Car #{i + 1} Model: ");
                 string model = myTI.ToTitleCase(Console.ReadLine());
 
-
+                int year = 0;
                 Console.Write($"Enter Car #{i + 1} Year: ");
-                int year = int.Parse(Console.ReadLine());
+                //while (true)
+                //{
+                //    try
+                //    {
+                        year = int.Parse(Console.ReadLine());
+                //        break;
+                //    }
+                //    catch (FormatException e)
+                //    {
+                //        Console.WriteLine("Please enter a number");
+                //    }
+                //}
 
+                double price = 0;
                 Console.Write($"Enter Car #{i + 1} Price: ");
-                double price = Convert.ToDouble(Console.ReadLine());
+                //try
+                //{
+                    price = int.Parse(Console.ReadLine());
+                    //break;
+                //}
+                //catch (FormatException e)
+                //{
+                //    Console.WriteLine("Please enter a number");
+                //}
+
                 Console.WriteLine();
                 Console.WriteLine();
 
                 cars.Add(new Car(make, model, year, price));
             }
 
+            int j = 1;
             Console.WriteLine("Current Inventory: ");
-            foreach (Car c in cars)
+     
+            for (int i = 0; i < cars.Count; i++)
             {
-                c.ToString();
+                Console.Write((i + 1) + ". ");
+                cars[i].ToString();
             }
 
+            int sel;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Which vehicle are you interested in?");
+                    sel = int.Parse(Console.ReadLine());
 
+                    Console.WriteLine(cars[sel - 1].ToString());
+                    //Try to apply to list
+                    break;
 
+                }
+                catch (ArgumentOutOfRangeException e)
+                {
+                    Console.WriteLine($"Range - Please enter a number from 1 - {cars.Count}");
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine($"Format -  Please enter a number from 1 - {cars.Count}");
 
+                }
+
+            }
+
+            // Section asking for purchasing
+            Console.WriteLine("Would you like to purchase this vehicle? (y/n)");
+            string confirm = Console.ReadLine();
+            while (confirm != "y" && confirm != "n" && confirm != "n" && confirm != "N")
+            {
+                Console.Write("Please enter either y or n: ");
+                confirm = Console.ReadLine();
+            }
+
+            if (confirm == "y" || confirm == "Y")
+            {
+                Console.WriteLine("Excellent! Our finance department will be in touch with you shortly");
+                cars.RemoveAt(sel - 1);
+
+                //Show the inventory again
+                Console.WriteLine("Current Inventory: ");
+         
+                for (int i = 0; i < cars.Count; i++)
+                {
+                    Console.Write((i+1) + ". ");
+                    cars[i].ToString();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Okay");
+                //TODO loop to do it again
+            }
         }
 
-        
     }
+    
 }
